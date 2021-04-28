6fabaa0a727eebe990236edb5b45f690a6f07a66
BBrownley <brendobrownley@yahoo.com>
2021-04-27 20:43:50 -0400

    Fixing deployed url

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <ScrollToTop />
       <Container>
         <Navbar />
